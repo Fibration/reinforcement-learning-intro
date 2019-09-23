16f7860be04885527d97e9c20105266b2cfbcb58
Joseph Chan <joseph6.626@gmail.com>
2019-09-23 15:16:13 +1000

    fixed type hints for self

--- algorithms_tabular/bandits.py
+++ algorithms_tabular/bandits.py
@@ -1,11 +1,13 @@
 import random
 
+
 # slot machine game for bandits
 class Game:
-    def __init__(self: Game):
-        self.arms = [random.uniform(-5,5) for i in range(10)]
+    def __init__(self):
+        self.arms = [random.uniform(-5, 5) for i in range(10)]
 
-    def play(self: Game, arm: int) -> float:
+    def play(self, arm: int) -> float:
         if arm < 9:
             return random.normalvariate(self.arms[arm], 1)
+
 

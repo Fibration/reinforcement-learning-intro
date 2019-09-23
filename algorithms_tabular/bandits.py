ff7aff7b1ae36063aaf2c0cc8a040b8fa572b933
Joseph Chan <joseph6.626@gmail.com>
2019-09-23 15:50:56 +1000

    impl greedy bandit

--- algorithms_tabular/bandits.py
+++ algorithms_tabular/bandits.py
@@ -13,6 +13,9 @@ class Game:
     def play(self, arm: int) -> float:
         if arm < 9:
             return random.normalvariate(self.arms[arm], 1)
+        else:
+            return random.normalvariate(self.arms[9], 1)
+        # force action to always be valid
 
 
 # plot performance
@@ -22,3 +25,19 @@ def plot_performance(rewards: List[float], title='Bandit Performance'):
     plt.ylabel('average reward')
     plt.title(title)
     plt.show()
+
+
+# Greedy bandit
+def greedy(steps: int):
+    game = Game()
+    Q = [0] * 10  # initialise value function
+    rewards = []  # keep track of performance
+
+    for step in range(1, steps + 1):
+        best = Q.index(max(Q))
+        reward = game.play(best)
+        Q[best] = (reward + (step - 1) * Q[best]) / step
+        rewards.push(rewards[-1] * (step - 1) / step + reward / step)
+
+    # plot what we get
+    plot_performance(rewards)

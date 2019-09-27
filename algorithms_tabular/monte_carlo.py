9694d6a0f031369630f2dd28c60a7f1acfcec860
Joseph Chan <joseph6.626@gmail.com>
2019-09-28 02:03:47 +1000

    made it use the blackjack game

--- algorithms_tabular/monte_carlo.py
+++ algorithms_tabular/monte_carlo.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Tuple, Callable
-from copy import deepcopy
-import random
+import sys
+sys.path.append('../')
+from games.blackjack import Blackjack
 
 # implement Monte Carlo algorithms for discrete action,
 # discrete state, episodic games
@@ -33,7 +34,7 @@ class Game:
         return sum(self.state) >= 18 or max(self.state) >= 9
 
 
-def mc_prediction(game: Game,
+def mc_prediction(game,
                   num_episodes: int,
                   pi: Callable[[Tuple[int]], int],
                   gamma: float) -> Dict[List[int], float]:
@@ -42,42 +43,51 @@ def mc_prediction(game: Game,
     Q = dict()
 
     for episode in range(num_episodes):
-        curr_game = deepcopy(game)
-        state_actions = []
+        curr_game = game()
+        states = []
         rewards = []
         while not curr_game.terminal:
             state = curr_game.get_state()
             action = pi(state)
-            state_actions.append((*state, action))
+            states.append(tuple(state))
             reward, _ = curr_game.play(action)
             rewards.append(reward)
 
         # estimate Q from episode
         G = 0
-        while len(state_actions) != 0:
+        while len(states) != 0:
             G = gamma * G + rewards.pop()
-            state_action = state_actions.pop()
-            if state_action not in set(state_actions):
-                if state_action in returns:
-                    returns_for_state = returns[state_action]
+            state = states.pop()
+            if state not in set(states):
+                if state in returns:
+                    returns_for_state = returns[state]
                     returns_for_state.append(G)
-                    returns[state_action] = returns_for_state
+                    returns[state] = returns_for_state
                 else:
-                    returns[state_action] = [G]
-                Q[state_action] = (sum(returns[state_action]) /
-                                   len(returns[state_action]))
+                    returns[state] = [G]
+                Q[state] = (sum(returns[state]) /
+                                   len(returns[state]))
 
     return Q
 
 
 def main():
-    game = Game()
-    Q = mc_prediction(game, 100, choose_rand, 0.5)
-    print(len(Q))
+    game = Blackjack
+    Q = mc_prediction(game, 1000, choose_rand, 0.5)
+    print(Q)
 
 
 def choose_rand(state: List[int]) -> int:
-    return random.randrange(4)
+    if state[3] == 0:
+        if state[2] > 19:
+            return 0
+        else:
+            return 1
+    else:
+        if state[2] > 29:
+            return 0
+        else:
+            return 1
 
 
 if __name__ == '__main__':
